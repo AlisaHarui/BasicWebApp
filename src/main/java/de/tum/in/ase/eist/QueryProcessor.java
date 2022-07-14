d4335891538d38e166357c18369168c807d33fb8
ge82kis <ge82kis@mytum.de>
2022-07-14 13:23:39 +0200

    addition zweier Zahlen aus dem String

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,28 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Ad";
-        } else {
-            return "";
+        } else if (query.contains("plus")) {
+            String[] numbers = query.split(" plus ");
+            char[] chars1 = numbers[0].toCharArray();
+            char[] chars2 = numbers[1].toCharArray();
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+
+            for (char c : chars1) {
+                if (Character.isDigit(c)) {
+                    sb1.append(c);
+                }
+            }
+
+            for (char c : chars2) {
+                if (Character.isDigit(c)) {
+                    sb2.append(c);
+                }
+            }
+//            int num1 = new Integer(sb1.toString());
+//            new Integer(sb2.toString());
+            return "" + sb1 + sb2;
         }
+        return "";
     }
 }
